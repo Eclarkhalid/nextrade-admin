c5c73c68df052f68be89aa5c2ea709643cfc68ab
Eclarkhalid <eclarkhalid@gmail.com>
2023-07-16 20:55:32 +0300

    Update delete to handle deleting the store

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -42,4 +42,35 @@ export async function PATCH(req: Request,
     console.log('[STORE_PATCH', error);
     return new NextResponse("Internal error", {status: 500});
   }
-}
+};
+
+//  delete method
+export async function DELETE(req: Request,
+  { params} : { params : {storeId: string}}
+  ) {
+  try {
+    const { userId} =auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", {status: 401});
+    }
+
+
+    if (!params.storeId) {
+      return new NextResponse("Store Id is required", {status: 400});
+    }
+
+    const store = await prismadb.store.deleteMany({
+      where: {
+        id: params.storeId,
+        userId
+      }
+    });
+
+    return NextResponse.json(store);
+
+  } catch (error) {
+    console.log('[STORE_DELETE', error);
+    return new NextResponse("Internal error", {status: 500});
+  }
+};
