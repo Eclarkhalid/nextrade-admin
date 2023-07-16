6f0c7c03addd4fda5c3bd53c529e2c49c3d2bcd0
Eclarkhalid <eclarkhalid@gmail.com>
2023-07-16 14:08:05 +0300

    Create the settings navbar

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,12 +1,37 @@
 "use client"
 
+import { cn } from "@/lib/utils"
+import Link from "next/link";
+import { useParams, usePathname } from "next/navigation";
+
 export function MainNavbar({
     className,
     ...props
 }: React.HTMLAttributes<HTMLElement>) {
+    const pathname = usePathname();
+    const params = useParams();
+    const routes = [{
+        // form
+        href:`/${params.storeId}/settings`,
+        label: 'Settings',
+        active: pathname === `/${params.storeId}/settings`,
+
+    }];
     return <>
-    <nav>
-        
+    <nav
+    className={cn('flex items-center space-x-4 lg:space-x-5', className)}
+    >
+        {routes.map((route) => (
+            <Link href={route.href}
+            key={route.href}
+            className={cn(
+                "text-sm font-medium transition-colors hover:text-primary",
+                route.active ? "text-black dark:text-white" : " text-muted-foreground"
+            )}
+            >
+            {route.label}
+            </Link>
+        ))}
     </nav>
     </>
 };
