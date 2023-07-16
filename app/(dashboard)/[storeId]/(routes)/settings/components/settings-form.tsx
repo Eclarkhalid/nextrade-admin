007213a5d385781519aa96549cdfb0a9e79b50b9
Eclarkhalid <eclarkhalid@gmail.com>
2023-07-16 19:54:54 +0300

    update settings form

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -9,9 +9,14 @@ interface SettingsFormProps {
 const SettingsForm: React.FC<SettingsFormProps> = ({
       initialData
 }) => {
-      return ( <>
-            <div>SettingsForm</div>
-      </> );
+      return <>
+      <div className="flex items-center justify-between">
+            <Heading
+            title="settings"
+            description="Manage Store Preferences"
+            />
+      </div>
+      </>
 }
  
 export default SettingsForm;
