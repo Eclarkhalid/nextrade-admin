b2bc28ca610c37457c4a48b339e70d7986683e58
Eclarkhalid <eclarkhalid@gmail.com>
2023-07-16 14:12:57 +0300

    Creating the store switcher

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -1,18 +1,5 @@
-'use client'
-
-import { Store } from "@prisma/client"
-import { PopoverTrigger } from "./ui/popover"
-
-type PopoverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>
-
-interface StoreSwitcherProps extends PopoverTriggerProps {
-    items: Store[];
-};
-
-export default function StoreSwitcher({
-    items = []
-}: StoreSwitcherProps) {
+export default function StoreSwitcher() {
     return <>
-    <div>Store Switcher</div>
+    <div>StoreSwitcher</div>
     </>
 }
