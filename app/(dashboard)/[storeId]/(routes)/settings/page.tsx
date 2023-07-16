da3606ce9ac738fc9c2964853a3e9678c38ec7b3
Eclarkhalid <eclarkhalid@gmail.com>
2023-07-16 19:41:21 +0300

    Update settings

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -1,4 +1,6 @@
+import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs"
+import { redirect } from "next/navigation";
 
 interface SettingsPageProps {
       params: {
@@ -6,12 +8,31 @@ interface SettingsPageProps {
       }
 };
 
-const SettingsPage: React.FC<SettingsPageProps> = ({
+const SettingsPage: React.FC<SettingsPageProps> = async ({
       params
 }) => {
       const { userId } = auth();
+
+      if (!userId) {
+            redirect('/sign-in');
+      }
+
+      const store = await prismadb.store.findFirst({
+            where: {
+            id: params.storeId,
+            userId
+            }
+      });
+
+      if (!store) {
+            redirect("/");
+      }
       return <>
-      <h1>Settings here</h1>
+      <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                  <SettingForm />
+            </div>
+      </div>
       </>
 }
  
